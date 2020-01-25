ebc4f568d51cea7d0adade075b6c0fe328b72f9d
Ricardo Canelas <ricardocanelas@gmail.com>
2020-01-25 13:02:52 -0300

    Updates

---
+++ precache-manifest.0a8c2110f87bd5c7b9863b8a55d93481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1740f7921c288e1b22b0e6426f7b76",
+    "url": "/redux-saga-promise-example/index.html"
+  },
+  {
+    "revision": "4d3d5581c9cbb1ade2fe",
+    "url": "/redux-saga-promise-example/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "4193bcc2c00e792acd14",
+    "url": "/redux-saga-promise-example/static/css/main.ff3194a9.chunk.css"
+  },
+  {
+    "revision": "4d3d5581c9cbb1ade2fe",
+    "url": "/redux-saga-promise-example/static/js/2.75bb9b25.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/redux-saga-promise-example/static/js/2.75bb9b25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4193bcc2c00e792acd14",
+    "url": "/redux-saga-promise-example/static/js/main.2e705e76.chunk.js"
+  },
+  {
+    "revision": "1309a27d5d41084894c0",
+    "url": "/redux-saga-promise-example/static/js/runtime-main.5510792f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a43fb617dbf17231025c27d899ce8384.js"
+  "/redux-saga-promise-example/precache-manifest.0a8c2110f87bd5c7b9863b8a55d93481.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-saga-promise-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
